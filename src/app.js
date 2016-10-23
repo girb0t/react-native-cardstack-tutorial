78475b833a57f8249cfca4440f14e15b5275d280
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-10-23 15:32:56 -0400

    set up redux boilerplate

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducers from './reducers';
+
+// Provider is the thing that binds redux with React
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </Provider>
   );
 };
 
