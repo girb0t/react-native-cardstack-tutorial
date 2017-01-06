82979c280aa4e647c142630038f8d55a9ec10aef
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-01-06 00:15:12 -0500

    cleanup

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,9 +1,8 @@
 export default (state = null, action) => {
-  debugger
   switch (action.type) {
-    case 'SELECT_LIBRARY':
-      return action.payload;
-    default:
-      return state;
+  case 'SELECT_LIBRARY':
+    return action.payload;
+  default:
+    return state;
   }
 };
